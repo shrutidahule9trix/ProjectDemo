daeb75e77a925c2c5c03a180652ce994693c5cf4
Shruti Dahule <shruti.dahule@navaantrix.com>
2025-07-17 18:02:55 +0530

    route modified

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,29 +1,31 @@
-/** @format */
 
+
+/** @format */
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-} from 'react-router-dom';
-import Index from '../Components/Pages/Entry/Index';
-import LoginPage from '../Components/Pages/Login/Login.jsx';
-import Organisation from '../Components/Pages/Organisation/Organisation.jsx';
-import AddUser from '../Components/Pages/AddUser/AddUser.jsx';
-import SystemAttribute from '../Components/Pages/SystemAttribute/SystemAttribute.jsx';
-import Pivot from '../Components/Pages/Work/Pivot.jsx';
+} from "react-router-dom";
 
+import Index from "../Components/Pages/Entry/Index";
+import LoginPage from "../Components/Pages/Login/Login.jsx";
+import Organisation from "../Components/Pages/Organisation/Organisation.jsx";
+import AddUser from "../Components/Pages/AddUser/AddUser.jsx";
+import SystemAttribute from "../Components/Pages/SystemAttribute/SystemAttribute.jsx";
+import PivotPage from "../Components/Pages/Pivot/Pivot.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Index />}>
-        <Route path="/organisation" element={<Organisation />} />
-        <Route path="/add-user" element={<AddUser />} />
-        <Route path="/system-attribute" element={<SystemAttribute />} />
-         <Route path="/pivot" element={<Pivot />} />
-      </Route>
+        <Route path="organisation" element={<Organisation />} />
+        <Route path="add-user" element={<AddUser />} />
+        <Route path="system-attribute" element={<SystemAttribute />} />
+        <Route path="pivot" element={<PivotPage />}/>
+        </Route>
+      
+
       <Route path="/login" element={<LoginPage />} />
-     
     </>
   )
 );
