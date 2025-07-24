3a39f90c05d475c611001abd7628ec924754facf
Shruti Dahule <shruti.dahule@navaantrix.com>
2025-07-24 11:10:28 +0530

    Organisation updated

--- src/Components/Pages/Organisation/Organisation.jsx
+++ src/Components/Pages/Organisation/Organisation.jsx
@@ -6,24 +6,22 @@ import Symbol from '../../../Assets/SVG/symbol';
 
 function PaperBox({ elevations = [20, 0, 8] }) {
   return (
-    <div>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          Color: "pink",
-          '& > :not(style)': { m: 1, width: 128, height: 128 },
-        }}
-      >
-        {elevations.map((e, i) => (
-          <Paper key={i} elevation={e} />
-        ))}
-      </Box>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: 'pink',
+        '& > :not(style)': { m: 1, width: 128, height: 128 },
+      }}
+    >
+      {elevations.map((e, i) => (
+        <Paper key={i} elevation={e} />
+      ))}
+    </Box>
   );
 }
 
-function TextFieldGroup() {
+function TextFieldGroup({ values, onChange }) {
   return (
     <Box
       component="form"
@@ -31,15 +29,32 @@ function TextFieldGroup() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        value={values.outlined}
+        onChange={(e) => onChange('outlined', e.target.value)}
+      />
+      <TextField
+        id="filled-basic"
+        label="Filled"
+        variant="filled"
+        value={values.filled}
+        onChange={(e) => onChange('filled', e.target.value)}
+      />
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        value={values.standard}
+        onChange={(e) => onChange('standard', e.target.value)}
+      />
     </Box>
   );
 }
 
-function AutoCompleteBox({ options = ['Option 1', 'Option 2'] }) {
-  const [value, setValue] = useState(options[0]);
+function AutoCompleteBox({ options, value, onChange }) {
   const [inputValue, setInputValue] = useState('');
 
   return (
@@ -48,7 +63,7 @@ function AutoCompleteBox({ options = ['Option 1', 'Option 2'] }) {
       <div>{`inputValue: '${inputValue}'`}</div>
       <Autocomplete
         value={value}
-        onChange={(event, newValue) => setValue(newValue)}
+        onChange={(event, newValue) => onChange(newValue)}
         inputValue={inputValue}
         onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
         id="controllable-states-demo"
@@ -60,7 +75,7 @@ function AutoCompleteBox({ options = ['Option 1', 'Option 2'] }) {
   );
 }
 
-function SymbolText({ text = 'JKDBFJRBFJBFJBFJ', heading = 'h1. Heading' }) {
+function SymbolText({ text, heading, onTextChange, onHeadingChange }) {
   return (
     <Box
       sx={{
@@ -71,25 +86,61 @@ function SymbolText({ text = 'JKDBFJRBFJBFJBFJ', heading = 'h1. Heading' }) {
       }}
     >
       <Symbol width="22" height="22" />
-      <Typography sx={{ fontFamily: 'monospace', fontSize: '0.9rem' }}>
-        {text}
-      </Typography>
-      <Typography variant="h1" component="h2" sx={{ fontSize: '30px' }}>
-        {heading}
-      </Typography>
+      <TextField
+        label="Text"
+        variant="standard"
+        value={text}
+        onChange={(e) => onTextChange(e.target.value)}
+        sx={{ width: '150px' }}
+      />
+      <TextField
+        label="Heading"
+        variant="standard"
+        value={heading}
+        onChange={(e) => onHeadingChange(e.target.value)}
+        sx={{ width: '150px' }}
+      />
     </Box>
   );
 }
 
 function Files() {
+  const [textFieldValues, setTextFieldValues] = useState({
+    outlined: '',
+    filled: '',
+    standard: '',
+  });
+
+  const handleTextFieldChange = (field, value) => {
+    setTextFieldValues((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const [autoValue, setAutoValue] = useState('Option 1');
+  const [symbolText, setSymbolText] = useState('Custom Text');
+  const [symbolHeading, setSymbolHeading] = useState('Custom Heading');
+  
+
   return (
     <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 4 }}>
       <PaperBox />
-      <TextFieldGroup />
-      <AutoCompleteBox />
-      <SymbolText text="Custom Text" heading="Custom Heading" />
+      <TextFieldGroup
+        values={textFieldValues}
+        onChange={handleTextFieldChange}
+      />
+      <AutoCompleteBox
+        options={['Option 1', 'Option 2']}
+        value={autoValue}
+        onChange={setAutoValue}
+      />
+      <SymbolText
+        text={symbolText}
+        heading={symbolHeading}
+        onTextChange={setSymbolText}
+        onHeadingChange={setSymbolHeading}
+      />
     </Box>
   );
 }
 
 export default Files;
+
